cace4d0b7ab1b08d6912e3a76cb1f101b4fc331e
Yashika414 <78353827+Yashika414@users.noreply.github.com>
2021-05-05 17:19:11 +0530

    Add files via upload

---
+++ testProject-main/testProject-main/sketch.js
@@ -0,0 +1,37 @@
+
+
+
+function setup() {
+  createCanvas(800,800);
+//  for (i = 1 ; i <= 10; i = i + 1)
+//  {
+//    console.log("yashika")
+//  }
+// for (i = 1 ; i <= 20 ; i = i + 1)
+// {
+//  console.log(i)
+// }
+// for (i = 2 ; i <= 20 ; i = i + 2)
+// {
+//   console.log(i)
+// }
+for (i = 1 ; i <= 50 ; i = i + 1)
+{
+  if (i%5 == 0)
+  {
+   console.log(i)
+  }
+}
+
+  
+}
+
+function draw() {
+  background("black");  
+ 
+  
+}
+
+
+
+
